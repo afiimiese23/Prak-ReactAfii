90bbf4e9a14a0b2a3f599eb8a250f23f1d5172d8
afiimiese23 <nur24si@mahasiswa.pcr.ac.id>
2026-04-20 20:47:52 +0700

    Pertemuan 6

--- src/pertemuan5/layouts/Sidebar.jsx
+++ src/pertemuan5/layouts/Sidebar.jsx
@@ -3,22 +3,17 @@ import { MdDashboardCustomize } from "react-icons/md";
 export default function Sidebar() {
     return (
         <div id="sidebar" className="flex min-h-screen w-90 flex-col bg-white p-10 shadow-lg">
-            {/* Logo */}
             <div id="sidebar-logo" className="flex flex-col">
                 <span id="logo-title" className="font-poppins text-[48px] text-gray-900">
-		                Sedap <b id="logo-dot" className="text-hijau">.</b>
-		            </span>
+		                Sedap <b id="logo-dot" className="text-hijau">.</b> </span>
                 <span id="logo-subtitle" className="font-semibold text-gray-400">Modern Admin Dashboard</span>
             </div>
 
-            {/* List Menu */}
             <div id="sidebar-menu" className="mt-10">
                 <ul id="menu-list" className="space-y-3">
                     <li className="group flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:text-hijau">
                         <MdDashboardCustomize className="mr-4 text-2xl group-hover:font-extrabold" />
-                        <div id="menu-1" className="group-hover:font-extrabold">
-                            Dashboard
-                        </div>
+                        <div id="menu-1" className="group-hover:font-extrabold"> Dashboard </div>
                     </li>
                     <li>
 	                    <div id="menu-2" className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold">Orders</div>
@@ -32,7 +27,6 @@ export default function Sidebar() {
                 </ul>
             </div>
 
-            {/* Footer */}
             <div id="sidebar-footer" className="mt-auto">
                 <div id="footer-card" className="bg-hijau px-4 py-2 rounded-md shadow-lg mb-10 flex items-center justify-between">
                     <div id="footer-text" className="text-white text-sm">
