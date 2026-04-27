308151e94d785cda1d106c0fa7fe8fe9c4317ca0
afiimiese23 <nur24si@mahasiswa.pcr.ac.id>
2026-04-27 16:24:53 +0700

    Pertemuan7

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import Orders from "./pages/Orders";
-import NotFound from "./pages/NotFound";
-import ErrorPage from "./pages/ErrorPage";
+
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Forgot from './pages/auth/Forgot';
+// import Loading from './components/Loading';
+
+const Dashboard = React.lazy(() => import("./pages/main/Dashboard"))
+const Orders = React.lazy(() => import("./pages/main/Orders"))
+const Customers = React.lazy(() => import("./pages/main/Customers"))
+const ErrorPage = React.lazy(() => import("./pages/main/ErrorPage"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+const Loading = React.lazy(() => import("./components/Loading"))
+
 
 export default function App() {
   return (
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-        <div id="layout-wrapper" className="flex flex-row flex-1">
-            <Sidebar/>
-            <div id="main-content" className="flex-1 p-4">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/orders" element={<Orders />} />
-                    <Route path="/customers" element={<Customers />} />
-                    
-                    {/* Error Routes */}
-                    <Route path="/error-400" element={<ErrorPage code="400" title="Bad Request" description="Permintaan tidak dapat diproses oleh server." image="https://illustrations.popsy.co/gray/falling.svg" />} />
-                    <Route path="/error-401" element={<ErrorPage code="401" title="Unauthorized" description="Anda harus login terlebih dahulu." image="https://illustrations.popsy.co/gray/shaking-hands.svg" />} />
-                    <Route path="/error-403" element={<ErrorPage code="403" title="Forbidden" description="Anda tidak memiliki akses ke halaman ini." image="https://illustrations.popsy.co/gray/stop.svg" />} />
-                    <Route path="*" element={<ErrorPage code="404" title="Page Not Found" description="Halaman yang anda cari raib entah kemana." />} />
-                </Routes>
-            </div>
-        </div>
-    </div>
+    <Suspense fallback={<Loading />}> 
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/customers" element={<Customers />} />
+            </Route>
+
+            <Route element={<AuthLayout/>}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="/forgot" element={<Forgot/>} />
+            </Route>  
+
+            {/* Error Routes */}
+            <Route path="/error-400" element={<ErrorPage code="400" title="Bad Request" description="Permintaan tidak dapat diproses oleh server." image="https://illustrations.popsy.co/gray/falling.svg" />} />
+            <Route path="/error-401" element={<ErrorPage code="401" title="Unauthorized" description="Anda harus login terlebih dahulu." image="https://illustrations.popsy.co/gray/shaking-hands.svg" />} />
+            <Route path="/error-403" element={<ErrorPage code="403" title="Forbidden" description="Anda tidak memiliki akses ke halaman ini." image="https://illustrations.popsy.co/gray/stop.svg" />} />
+            <Route path="*" element={<ErrorPage code="404" title="Page Not Found" description="Halaman yang anda cari raib entah kemana." />} />
+        </Routes>
+    </Suspense>
   );
 }
